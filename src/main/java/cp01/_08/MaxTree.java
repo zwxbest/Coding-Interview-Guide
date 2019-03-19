02a438282a978d0ffd1ccc91f261e6ea487f7fe3
zwxbest <zhangweixiao@live.com>
2019-03-19 23:44:26 +0800

    add

--- src/main/java/cp01/_08/MaxTree.java
+++ src/main/java/cp01/_08/MaxTree.java
@@ -31,42 +31,79 @@ public class MaxTree {
     Map<Node, Node> lBigMap = new HashMap<>();
     Map<Node, Node> rBigMap = new HashMap<>();
 
-    public Node getMaxTree(int[] arr) {
+    public Node getMaxTree1(int[] arr) {
         //初始化
         Node[] nodes = new Node[arr.length];
         for (int i = 0; i < arr.length; i++) {
             nodes[i] = new Node(arr[i]);
         }
         for (int i = 0; i < arr.length; i++) {
-            Node curNode =nodes[i];
-            //每次上一个元素
-            while (!stack.isEmpty() && stack.peek().getValue() < curNode.getValue()){
-                setMap(stack,lBigMap);
+            Node curNode = nodes[i];
+            if(stack.isEmpty()){
+                lBigMap.put(curNode,null);
+            }else {
+                Node pop = stack.pop();
+                if(pop.getValue() < curNode.getValue()){
+                    lBigMap.put(curNode,null);
+                }else {
+                    lBigMap.put(curNode,pop);
+                }
             }
             stack.push(curNode);
         }
-        while (!stack.isEmpty()){
-            setMap(stack,lBigMap);
+        stack.clear();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            Node curNode = nodes[i];
+            if(stack.isEmpty()){
+                rBigMap.put(curNode,null);
+            }else{
+                Node pop = stack.pop();
+                if(pop.getValue() < curNode.getValue()){
+                    rBigMap.put(curNode,null);
+                }else {
+                    rBigMap.put(curNode,pop);
+                }
+            }
+            stack.push(curNode);
         }
-        return null;
-    }
-
-    private void  setMap(Stack<Node> stack,Map<Node,Node> map){
-        //当前要处理的node
-        Node node = stack.pop();
-        //看看左边是否有比node大的。
-        if(stack.isEmpty()){
-            map.put(node,null);
-        }else {
-            map.put(node,stack.peek());
+        //根据rBigMap和lBigMap构造二叉树
+        Node head = null;
+        for (int i = 0; i < arr.length; i++) {
+            Node curNode = nodes[i];
+            Node lParent = lBigMap.get(curNode);
+            Node rParent = rBigMap.get(curNode);
+            if(lParent == null && rParent == null){
+                head = curNode;
+            } else if(lParent == null){
+                if(rParent.left == null){
+                    rParent.left = curNode;
+                }else {
+                    rParent.right = curNode;
+                }
+            }else if(rParent == null){
+                if(lParent.left == null){
+                    lParent.left = curNode;
+                }else {
+                    lParent.right = curNode;
+                }
+            }else {
+                Node parent = lParent.getValue()<rParent.getValue()?lParent:rParent;
+                if(parent.left == null){
+                    parent.left = curNode;
+                }else {
+                    parent.right = curNode;
+                }
+            }
         }
+        return head;
     }
 
     public static void main(String[] args) {
         MaxTree maxTree = new MaxTree();
 //        maxTree.getMaxTree(new int[]{1,2,3,4,5});
 //        maxTree.getMaxTree(new int[]{5,4,3,2,1});
-          maxTree.getMaxTree(new int[]{5,4,3,2,6});
+        MaxTree maxTree1 = new MaxTree();
+        Node maxTree11 = maxTree1.getMaxTree1(new int[]{5, 4, 3, 2, 6});
         System.out.println(1);
     }
 }

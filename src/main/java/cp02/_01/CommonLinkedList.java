a1b96603eb1ddbe835d064ef253545274603d96b
zhangweixiao <zhangweixiao@live.com>
2019-03-22 12:51:30 +0800

    add

---
+++ src/main/java/cp02/_01/CommonLinkedList.java
@@ -0,0 +1,35 @@
+package cp02._01;
+
+/**
+ * 有序列表,从头到尾,从小到大
+ *
+ * @author zhangweixiao - 19-3-22
+ */
+public class CommonLinkedList {
+
+    public Node getCommonLinkedList(Node head1, Node head2) {
+
+        Node curNode = null;
+        Node head = null;
+
+        while (head1 != null && head2 != null) {
+            if (head1.value < head2.value) {
+                head1 = head1.next;
+            } else if (head1.value > head2.value) {
+                head2 = head2.next;
+            } else {
+                if (curNode == null) {
+                    curNode = new Node(head1.value);
+                    head = curNode;
+                } else {
+                    curNode.next = new Node(head1.value);
+                    curNode = curNode.next;
+                }
+                head1 = head1.next;
+                head2 = head2.next;
+            }
+        }
+        return head;
+
+    }
+}
---
+++ src/main/java/cp02/_01/Node.java
@@ -0,0 +1,14 @@
+package cp02._01;
+
+/**
+ * @author zhangweixiao - 19-3-22
+ */
+public class Node {
+
+    public int value;
+    public Node next;
+
+    public Node(int value) {
+        this.value = value;
+    }
+}

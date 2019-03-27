9e5c14126fa4d6fec94a989b0ba32b037f9939a9
zhangweixiao <zhangweixiao@live.com>
2019-03-27 20:59:50 +0800

    add

---
+++ src/main/java/cp02/_06/JosephusCircle.java
@@ -0,0 +1,37 @@
+package cp02._06;
+
+import cp02.node.Node;
+
+/**
+ * 环形单链表的约瑟夫问题
+ *
+ * @author zhangweixiao - 19-3-27
+ */
+public class JosephusCircle {
+
+    public Node josephusKill(Node head, int m) {
+        if (head == null || head.next == head || m < 1) {
+            return head;
+        }
+
+        //找到最后一个节点,因为如果m=1,那第一个报数的就死掉了,最后一个连接第二个重做环
+        Node last = head;
+        while (last.next != head) {
+            last = last.next;
+        }
+        int count = 0;
+        while (head != last) {
+            //因为从last开始,先++
+            if (++count == m) {
+                //删除next后,再连成环形
+                last.next = head.next;
+                count = 0;
+            } else {
+                last = last.next;
+            }
+            //head作为last的下一个节点
+            head = last.next;
+        }
+        return head;
+    }
+}
---
+++ src/test/java/cp02/_06/JosephusCircleTest.java
@@ -0,0 +1,59 @@
+package cp02._06;
+
+import base.BaseTest;
+import cp02.node.Node;
+import org.junit.Test;
+
+/**
+ * @author zhangweixiao - 19-3-27
+ */
+public class JosephusCircleTest extends BaseTest{
+
+    JosephusCircle josephusCircle = new JosephusCircle();
+
+    @Test
+    public void test(){
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = head;
+
+        Node node = josephusCircle.josephusKill(head, 2);
+        assertEqual(node.value,1);
+        assertEqual(node.next,node);
+    }
+
+    /**
+     * 以1计算
+     */
+    @Test
+    public void test1(){
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = head;
+
+        Node node = josephusCircle.josephusKill(head, 1);
+        assertEqual(node.value,4);
+    }
+
+    /**
+     * 以3计算
+     */
+    @Test
+    public void test2(){
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = head;
+
+        Node node = josephusCircle.josephusKill(head, 3);
+        assertEqual(node.value,1);
+    }
+}
